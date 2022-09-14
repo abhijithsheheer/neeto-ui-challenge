9d3b36474d040af207ef749c45167d7eee17602f
Abhijith Sheheer <89389135+abhijithsheheer@users.noreply.github.com>
2022-09-14 13:28:14 +0530

    Added list contact UI

---
+++ app/javascript/src/components/Dashboard/Contacts/index.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+import { Button } from "neetoui";
+import { Container, Header } from "neetoui/layouts";
+
+import MenuBar from "./MenuBar";
+import Table from "./Table";
+
+const Contacts = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <>
+      <MenuBar />
+      <Container>
+        <Header
+          menuBarToggle={() => {}}
+          title="All Contacts"
+          actionBlock={
+            <Button
+              icon="ri-add-line"
+              label="Add New Contact"
+              onClick={() => {}}
+            />
+          }
+          searchProps={{
+            value: searchTerm,
+            onChange: e => setSearchTerm(e.target.value),
+            placeholder: "Search Name, Email, Phone Number, Etc.",
+          }}
+        />
+        <Table />
+      </Container>
+    </>
+  );
+};
+
+export default Contacts;
