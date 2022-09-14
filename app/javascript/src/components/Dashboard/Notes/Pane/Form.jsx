ee031656f5aa51803e059fda7825872766ff1082
Abhijith Sheheer <89389135+abhijithsheheer@users.noreply.github.com>
2022-09-14 13:27:50 +0530

    Fixed validation for Note's Pane

--- app/javascript/src/components/Dashboard/Notes/Pane/Form.jsx
+++ app/javascript/src/components/Dashboard/Notes/Pane/Form.jsx
@@ -55,12 +55,13 @@ const NoteForm = ({ onClose, refetch, note, isEdit }) => {
             <Select
               required
               className="w-full flex-grow-0"
-              label="Assigned Contact"
-              name="assignedContact"
+              label="Assigned Role"
+              name="role"
               options={ROLES}
               placeholder="Select Role"
             />
             <Select
+              isMulti
               required
               className="w-full flex-grow-0"
               label="Tags"
